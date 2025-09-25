51ab8f9daffbe59abd7454bd241783896973886d
QIXIAOXIANSHENG <20020222chen@sina.com>
2025-09-24 20:19:17 -0500

    0.2.1, updated the calculation of lambda especially for suni based methods.

---
+++ src/getLambda.f90
@@ -0,0 +1,62 @@
+SUBROUTINE getlambda(nobs, nvars, nlam,ju,ulam,x, y, pf, flmin)
+    IMPLICIT NONE
+    DOUBLE PRECISION, PARAMETER :: big = 9.9E30
+    DOUBLE PRECISION, PARAMETER :: mfl = 1.0E-6
+
+    INTEGER :: nobs
+    INTEGER :: nvars
+    INTEGER :: nlam
+    INTEGER :: ju(nvars)
+
+    DOUBLE PRECISION :: x(nobs, nvars)
+    DOUBLE PRECISION :: y(nobs)
+    DOUBLE PRECISION :: ulam(nlam)
+    DOUBLE PRECISION :: flmin
+    DOUBLE PRECISION :: pf(nvars)
+
+    INTEGER :: l,j
+    DOUBLE PRECISION :: u
+    DOUBLE PRECISION:: alf
+    DOUBLE PRECISION :: al
+    DOUBLE PRECISION :: altemp(nlam)
+
+
+    IF (flmin < 1.0D0) THEN
+        flmin = MAX(mfl, flmin)
+        alf = flmin**(1.0D0/(DBLE(nlam) - 1.0D0))
+    END IF
+
+    DO l = 1, nlam
+        ! -------- COMPUTING LAMBDA -------- !
+        IF (flmin >= 1.0D0) THEN
+            al = ulam(l)
+            altemp(l) = al
+        ELSE
+            IF (l > 2) THEN
+                al = al*alf
+                altemp(l) = al
+            ELSE IF (l == 1) THEN
+                al = big
+                altemp(l) = al
+            ELSE IF (l == 2) THEN
+                al = 0.0D0
+                DO j = 1, nvars
+                    IF (ju(j) /= 0) THEN
+                        IF (pf(j) > 0.0D0) THEN
+                            u = DOT_PRODUCT(y, x(:, j))
+                            al = MAX(al, ABS(u)/pf(j))
+                        END IF
+                    END IF
+                END DO
+                al = al*alf/nobs
+                altemp(l) = al
+            END IF
+        END IF
+    END DO
+
+    IF (flmin < 1.0D0) THEN
+        ulam = altemp
+        altemp = LOG(altemp(nlam))
+        ulam(1) = exp(2 * altemp(2) - altemp(3))
+    END IF
+END SUBROUTINE getlambda
